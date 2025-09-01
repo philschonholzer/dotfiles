c559c73395ac1ddb4ac34b9f2f8ce8c74f9fce51
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2025-09-01 13:11:46 +0200

    Don't need buildFHSEnv

--- home.nix
+++ home.nix
@@ -10,39 +10,23 @@
     sha256 = "02104v34p84yn1fpqy0pcy12b5a1vi1drb4sfarcq8d9da8hywp0";
   };
 
-  tableplusEnv = pkgs.buildFHSEnv {
-    name = "tableplus";
-    targetPkgs = pkgs:
-      with pkgs; [
-        glib
-        gnutls
-        libsecret
-      ];
-    runScript = "${pkgs.appimage-run}/bin/appimage-run ${tableplusAppImage}";
-  };
+  tableplusEnv = pkgs.writeShellScriptBin "tableplus" ''
+    exec ${pkgs.appimage-run}/bin/appimage-run ${tableplusAppImage} "$@"
+  '';
 
   kdriveAppImage = pkgs.fetchurl {
     url = "https://download.storage.infomaniak.com/drive/desktopclient/kDrive-3.7.5.20250812-amd64.AppImage";
     sha256 = "1l54a0gzi499d3zgq4v9wrp7497n440sibv0dgycm3li3hqi2nya";
   };
-  kdriveEnv = pkgs.buildFHSEnv {
-    name = "kdrive";
-    targetPkgs = pkgs:
-      with pkgs; [
-        glib
-        gnutls
-        libsecret
-      ];
-    runScript = ''
-      # Force X11 so Qt doesn’t explode under Wayland/Hyprland
-      export XDG_SESSION_TYPE=x11
-      export QT_QPA_PLATFORM=xcb
-      unset WAYLAND_DISPLAY
-      unset SESSION_MANAGER
+  kdriveEnv = pkgs.writeShellScriptBin "kdrive" ''
+    # Force X11 so Qt doesn’t explode under Wayland/Hyprland
+    export XDG_SESSION_TYPE=x11
+    export QT_QPA_PLATFORM=xcb
+    unset WAYLAND_DISPLAY
+    unset SESSION_MANAGER
 
-      exec ${pkgs.appimage-run}/bin/appimage-run ${kdriveAppImage} "$@"
-    '';
-  };
+    exec ${pkgs.appimage-run}/bin/appimage-run ${kdriveAppImage} "$@"
+  '';
 in {
   home = {
     username = "philip";
