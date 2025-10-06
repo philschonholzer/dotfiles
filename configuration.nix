f763fc15abe07b2705ccb61461d12be5028bc9e4
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2025-10-06 18:05:17 +0200

    Allow localsend in firewall

--- configuration.nix
+++ configuration.nix
@@ -149,6 +149,8 @@
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
+  networking.firewall.allowedTCPPorts = [53317];
+  networking.firewall.allowedUDPPorts = [53317];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
