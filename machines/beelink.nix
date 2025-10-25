5c418a7743ea87b94cbc1b7a1f2d28467a432ba7
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2025-10-25 10:52:59 +0200

    Added portal, styled fuzzel and made niri conf out of store symlink

--- machines/beelink.nix
+++ machines/beelink.nix
@@ -1,6 +1,7 @@
 inputs: {
   pkgs,
   lib,
+  config,
   ...
 }: {
   imports = [
@@ -53,16 +54,53 @@ inputs: {
       gftp
       parsec-bin
       swaybg
+      xwayland-satellite
     ];
   };
 
   programs.alacritty.enable = true;
-  programs.fuzzel.enable = true;
+  programs.fuzzel = {
+    enable = true;
+    settings = {
+      main = {
+        lines = 10;
+        prompt = "  ";
+        inner-pad = 16;
+        width = 30;
+        font = "Sans\:size=18";
+        line-height = 30;
+      };
+
+      colors = {
+        background = "2e3440ff"; # Background - Nord0 (Darkest polar night)
+        text = "d8dee9ff"; # Text - Nord4 (Snow storm)
+        prompt = "4c566aff"; # Prompt - Nord3 (Lighter polar night - lower contrast)
+        placeholder = "4c566aff"; # Placeholder - Nord3 (Lighter polar night)
+        input = "8fbcbbff"; # Input text - Nord7 (Frost - better contrast)
+        match = "ebcb8bff"; # Match highlighting - Nord13 (Aurora yellow)
+        selection = "3b4252ff"; # Selection background - Nord1 (Polar night)
+        selection-text = "eceff4ff"; # Match in selection - Nord15 (Aurora purple)
+        counter = "4c566aff"; # Counter - Nord3 (Lighter polar night)
+        border = "4c566aff"; # Border - Nord3 (Lightest polar night)
+      };
+
+      border = {
+        width = 1;
+        radius = 12;
+      };
+    };
+  };
   programs.swaylock.enable = true;
   services.mako.enable = true;
   services.swayidle.enable = true;
   services.polkit-gnome.enable = true;
 
   # Niri configuration
-  xdg.configFile."niri/config.kdl".source = ../home-manager/desktop-manager/niri.kdl;
+  xdg.configFile."niri/config.kdl".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos-config/home-manager/desktop-manager/niri.kdl";
+
+  xdg.portal = {
+    enable = true;
+    config.common.default = "*";
+    extraPortals = [pkgs.xdg-desktop-portal-gnome];
+  };
 }
