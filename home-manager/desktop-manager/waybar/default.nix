b073ef234f96ea077e8f6ebdad0d9cf64d66cebb
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2025-10-31 22:01:38 +0100

    Added calendar tooltip to waybar clock

--- home-manager/desktop-manager/waybar/default.nix
+++ home-manager/desktop-manager/waybar/default.nix
@@ -106,9 +106,29 @@ in {
           max-length = 10;
         };
         clock = {
-          format = "{:%a %d.%m. %H:%M}";
-          format-alt = "{:%d %b W%V %Y}";
-          tooltip = false;
+          format = "{:%a. %d.%m. KW%V %H:%M}";
+          tooltip-format = "<tt><small>{calendar}</small></tt>";
+          tooltip = true;
+          calendar = {
+            mode = "year";
+            mode-mon-col = 3;
+            weeks-pos = "right";
+            on-scroll = 1;
+            format = {
+              months = "<span color='#ffead3'><b>{}</b></span>";
+              days = "<span color='#ecc6d9'><b>{}</b></span>";
+              weeks = "<span color='#99ffdd'><b>W{}</b></span>";
+              weekdays = "<span color='#ffcc66'><b>{}</b></span>";
+              today = "<span color='#ff6699'><b><u>{}</u></b></span>";
+            };
+          };
+          actions = {
+            on-click-right = "mode";
+            # on-scroll-up = "tz_up";
+            # on-scroll-down = "tz_down";
+            on-scroll-up = "shift_up";
+            on-scroll-down = "shift_down";
+          };
         };
         network = {
           format-icons = ["󰤯" "󰤟" "󰤢" "󰤥" "󰤨"];
