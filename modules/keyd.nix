74e4f957e58fb6afa470b69aa3491f663b64ccd8
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2025-11-01 16:38:32 +0100

    Use timeout in keyd

--- modules/keyd.nix
+++ modules/keyd.nix
@@ -5,14 +5,14 @@
       ids = ["*"];
       settings = {
         main = {
-          j = "overload(meta, j)";
-          f = "overload(meta, f)";
-          k = "overload(shift, k)";
-          d = "overload(shift, d)";
-          l = "overload(control, l)";
-          s = "overload(control, s)";
-          semicolon = "overload(alt, semicolon)";
-          a = "overload(alt, a)";
+          j = "overloadt2(meta, j, 200)";
+          f = "overloadt2(meta, f, 200)";
+          k = "overloadt2(shift, k, 200)";
+          d = "overloadt2(shift, d, 200)";
+          l = "overloadt2(control, l, 200)";
+          s = "overloadt2(control, s, 200)";
+          semicolon = "overloadt2(alt, semicolon, 200)";
+          a = "overloadt2(alt, a, 200)";
         };
       };
     };
