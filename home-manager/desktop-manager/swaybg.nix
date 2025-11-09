7692a2ca72ee63b14ca7ad8d91d5dc6589ba3558
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2025-11-09 11:43:47 +0100

    Start swaybg a systemd

--- home-manager/desktop-manager/swaybg.nix
+++ home-manager/desktop-manager/swaybg.nix
@@ -5,5 +5,24 @@
       recursive = true;
     };
   };
+
   home.packages = [pkgs.swaybg];
+
+  systemd.user.services.swaybg = {
+    Unit = {
+      Description = "swaybg wallpaper";
+      PartOf = ["graphical-session.target"];
+      After = ["graphical-session.target"];
+      Requisite = ["graphical-session.target"];
+    };
+
+    Service = {
+      ExecStart = "${pkgs.swaybg}/bin/swaybg -i /home/philip/Pictures/Wallpapers/kanagawa-blend.jpg";
+      Restart = "always";
+    };
+
+    # Install = {
+    #   WantedBy = ["graphical-session.target"];
+    # };
+  };
 }
