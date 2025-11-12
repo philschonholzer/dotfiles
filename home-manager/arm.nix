d1ab1fafbff84e05a33d9d4cfc499ec70357b1bb
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2025-11-12 11:27:16 +0100

    portal is defined in module

--- home-manager/arm.nix
+++ home-manager/arm.nix
@@ -143,25 +143,4 @@
 
   # Niri configuration
   xdg.configFile."niri/config.kdl".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos-config/home-manager/desktop-manager/niri-m.kdl";
-
-  xdg.portal = {
-    enable = true;
-    extraPortals = with pkgs; [
-      xdg-desktop-portal-gtk
-      xdg-desktop-portal-gnome
-    ];
-    config = {
-      common = {
-        default = ["gtk"];
-      };
-      niri = {
-        default = [
-          "gtk"
-          "gnome"
-        ];
-        "org.freedesktop.impl.portal.ScreenCast" = ["gnome"];
-        "org.freedesktop.impl.portal.Screenshot" = ["gnome"];
-      };
-    };
-  };
 }
