1e0b6eb17af3e0e35199cc20fa2425c5681d5824
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2025-11-25 19:20:14 +0100

    Use correct command to nixos-rebuild switch

--- home-manager/apps/zsh.nix
+++ home-manager/apps/zsh.nix
@@ -20,7 +20,7 @@
       p = "pnpm";
       mysql-proxy-apptiva = "cloud-sql-proxy kubernetes-283408:europe-west6:apptiva-mysql-8-common -p 3308";
       v = "c && vi .";
-      nrs = "sudo nixos-rebuild switch";
+      nrs = "sudo nixos-rebuild switch --flake .#$HOST";
       lg = "lazygit";
     };
   };
