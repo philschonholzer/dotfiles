90f3d5aee66d70e43f384dac74340d5f9e28d27f
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2025-12-02 17:07:05 +0100

    Added wlavu as a systemd unit

--- home-manager/desktop-manager/wlavu.nix
+++ home-manager/desktop-manager/wlavu.nix
@@ -1,3 +1,21 @@
 {wlavu, ...}: {
   home.packages = [wlavu];
+
+  systemd.user.services.wlavu = {
+    Unit = {
+      Description = "Wlavu audio visualizer";
+      After = ["graphical-session.target"];
+      PartOf = ["graphical-session.target"];
+    };
+
+    Service = {
+      ExecStart = "${wlavu}/bin/wlavu -a right -w 2";
+      Restart = "on-failure";
+      RestartSec = 3;
+    };
+
+    Install = {
+      WantedBy = ["graphical-session.target"];
+    };
+  };
 }
