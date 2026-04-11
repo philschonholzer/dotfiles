054b9e5421140f7d3e236baa574515817fdc8ab7
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2026-04-11 15:53:03 +0200

    feat: added local llms to opencode providers

--- home-manager/apps/opencode.nix
+++ home-manager/apps/opencode.nix
@@ -1,4 +1,5 @@
-{pkgs, ...}: {
+{ pkgs, ... }:
+{
   programs.opencode = {
     enable = true;
     package = pkgs.unstable.opencode;
@@ -11,8 +12,28 @@
           "git push*" = "deny";
         };
       };
+      "provider" = {
+        "lmstudio" = {
+          "npm" = "@ai-sdk/openai-compatible";
+          "name" = "LM Studio (local)";
+          "options" = {
+            "baseURL" = "http://192.168.1.65:1234/v1";
+          };
+          "models" = {
+            "google/gemma-4-31b" = {
+              "name" = "Gemma 4 31b";
+            };
+            "google/gemma-4-e4b" = {
+              "name" = "Gemma 4 e4b";
+            };
+            "google/gemma-4-26b-a4b" = {
+              "name" = "Gemma 26b A4B";
+            };
+          };
+        };
+      };
     };
   };
 
-  programs.zsh.shellAliases.oc = "opencode";
+  programs.zsh.shellAliases.oc = "opencode --port";
 }
