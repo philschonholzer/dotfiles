765e942d6fea17d144436c216436476f716be44a
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2026-06-12 10:11:56 +0200

    feat: discover models in oc

--- home-manager/apps/opencode.nix
+++ home-manager/apps/opencode.nix
@@ -14,13 +14,24 @@
       };
       "plugin" = [
         "opencode-lmstudio@latest"
+        "opencode-models-discovery"
       ];
       "provider" = {
         "lmstudio" = {
           "npm" = "@ai-sdk/openai-compatible";
-          "name" = "LM Studio (local)";
+          "name" = "LM Studio (MacMini)";
           "options" = {
             "baseURL" = "http://192.168.1.65:1234/v1";
+          };
+        };
+        "ollama" = {
+          "npm" = "@ai-sdk/openai-compatible";
+          "name" = "Ollama (MacMini)";
+          "options" = {
+            "baseURL" = "http://192.168.1.65:11434/v1";
+            "modelsDiscovery" = {
+              "enabled" = true;
+            };
           };
         };
       };
