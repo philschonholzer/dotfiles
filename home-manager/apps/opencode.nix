2e1e4ce68fdeccc0c4faa236cd8cfdf0da5a3c31
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2026-06-16 11:44:38 +0200

    chore: removed lm-studio discovery from opencode

--- home-manager/apps/opencode.nix
+++ home-manager/apps/opencode.nix
@@ -13,17 +13,9 @@
         };
       };
       "plugin" = [
-        "opencode-lmstudio@latest"
         "opencode-models-discovery"
       ];
       "provider" = {
-        "lmstudio" = {
-          "npm" = "@ai-sdk/openai-compatible";
-          "name" = "LM Studio (MacMini)";
-          "options" = {
-            "baseURL" = "http://192.168.1.65:1234/v1";
-          };
-        };
         "ollama" = {
           "npm" = "@ai-sdk/openai-compatible";
           "name" = "Ollama (MacMini)";
