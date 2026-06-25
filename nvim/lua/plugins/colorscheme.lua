963e8cd198e87255d73c8a0f4d8bd446db19a3de
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2026-06-25 09:01:52 +0200

    feat: refined desktop with blur

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,5 +1,5 @@
 return {
-  { "rebelot/kanagawa.nvim", lazy = false, name = "kanagawa", priority = 1000 },
+  { "rebelot/kanagawa.nvim", lazy = false, name = "kanagawa", priority = 1000, opts = { transparent = true } },
   {
     "LazyVim/LazyVim",
     opts = {
