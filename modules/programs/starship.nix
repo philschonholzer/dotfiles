02298a424cc9b785c6c05f51250dfa3fb7ee3577
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2026-08-04 11:55:10 +0200

    feat: show if npm or pnpm is used in project

--- modules/programs/starship.nix
+++ modules/programs/starship.nix
@@ -8,6 +8,13 @@
         gcloud = {
           disabled = true;
         };
+        custom.pkgmanager = {
+          command = ''if [ -f pnpm-lock.yaml ]; then echo "pnpm"; elif [ -f package-lock.json ]; then echo "npm"; elif [ -f yarn.lock ]; then echo "yarn"; elif [ -f bun.lockb ] || [ -f bun.lock ]; then echo "bun"; fi'';
+          when = "test -f package.json";
+          symbol = "📦 ";
+          style = "bold blue";
+          format = "[$symbol$output]($style) ";
+        };
       };
     };
   };
