95846702132660c3cf60044f6a91609d4b8c2046
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2026-08-05 16:53:44 +0200

    feat: allow wake on lan

--- modules/hosts/beelink/hardware.nix
+++ modules/hosts/beelink/hardware.nix
@@ -44,6 +44,7 @@
       ];
 
       networking.useDHCP = lib.mkDefault true;
+      networking.interfaces.enp1s0.wakeOnLan.enable = true;
 
       nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
       hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
