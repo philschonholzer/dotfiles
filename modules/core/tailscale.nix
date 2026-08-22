20ac5eed98105e6aeda7a35b8952e5937602a016
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2026-08-22 10:42:18 +0200

    feat: use service to get tailscale files

--- modules/core/tailscale.nix
+++ modules/core/tailscale.nix
@@ -6,18 +6,18 @@
   flake.modules.nixos.base = { pkgs, ... }: {
     services.tailscale.enable = true;
 
-    # systemd.services.tailscale-file-get = {
-    #   description = "Tailscale Taildrop file receiver";
-    #   after = [
-    #     "tailscaled.service"
-    #     "tailscaled-autoconnect.service"
-    #   ];
-    #   wantedBy = [ "multi-user.target" ];
-    #   serviceConfig = {
-    #     ExecStart = "${pkgs.tailscale}/bin/tailscale file get --loop /home/philip/Downloads";
-    #     Restart = "on-failure";
-    #     User = "philip";
-    #   };
-    # };
+    systemd.services.tailscale-file-get = {
+      description = "Tailscale Taildrop file receiver";
+      after = [
+        "tailscaled.service"
+        "tailscaled-autoconnect.service"
+      ];
+      wantedBy = [ "multi-user.target" ];
+      serviceConfig = {
+        ExecStart = "${pkgs.tailscale}/bin/tailscale file get --loop /home/philip/Downloads";
+        Restart = "on-failure";
+        User = "philip";
+      };
+    };
   };
 }
