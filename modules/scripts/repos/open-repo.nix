28462e748842c497ef22c3c576da44e5ec4feabc
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2026-09-21 12:18:16 +0200

    feat(repos): use noctalia for repository selection

--- modules/scripts/repos/open-repo.nix
+++ modules/scripts/repos/open-repo.nix
@@ -5,11 +5,16 @@
     {
       home.file."open-repo" = lib.mkIf pkgs.stdenv.isLinux {
         text = ''
-          dir=$(
+          if ! selection=$(
             fd -H -d 3 -t d -g '.git' ~/Projects ~/nixos-config --exec dirname |
               awk -F'/' '{short=substr($0, index($0,$5)); print short "\t" $0}' |
-                fuzzel -d --with-nth=1 --accept-nth=2
-          )
+                noctalia dmenu -p "Open repository"
+          ); then
+            echo "No selection made."
+            exit 1
+          fi
+
+          dir="''${selection#*$'\t'}"
 
           if [ -z "$dir" ]; then
             echo "No selection made."
